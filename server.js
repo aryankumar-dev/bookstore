af7e60b74ffbe1e04be10d4673f37d8a23788174
aryanaryan <103895665+aryankumar-dev@users.noreply.github.com>
2025-07-03 12:02:42 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
+import bookRoutes from './routes/books.js';
+import { logger } from './middleware/logger.js';
+import { authenticate } from './middleware/auth.js';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(logger);
+
+app.use('/', authRoutes);
+app.use('/books', authenticate, bookRoutes);
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
